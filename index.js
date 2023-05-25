fd910dd392af5c4ff28708e75e95cd8b875199a6
Suraj <suraj.c@copperdigital.com>
2023-05-25 17:11:33 +0530

    stablising connection

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//requiring connection
+const con = require("./Config/Config");
+
+// port name
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
