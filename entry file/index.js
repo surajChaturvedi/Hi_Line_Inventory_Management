798c40bc496bf5f09efb260beb358c7fc4bafd95
Suraj <suraj.c@copperdigital.com>
2023-05-25 12:04:30 +0530

    added entry file

--- entry file/index.js
+++ entry file/index.js
@@ -1,1 +1,12 @@
-hello 
+const express=require('express');
+const app=express();
+
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const port=process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+})
