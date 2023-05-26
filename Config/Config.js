74d50f35e0439b658f63219bf327c55de25f6029
Rashid <rashid.h@copperdigital.com>
2023-05-26 11:23:10 +0530

    changes

--- Config/Config.js
+++ Config/Config.js
@@ -1,28 +1,28 @@
-const {Sequelize, DataTypes} = require ('sequelize');
-// connection stablising with mysql
-const sequelize = new Sequelize("inventory", "root", "mysql", {
-    host: "localhost",
-    dialect: "mysql",
-    //port: "3307",
-    logging: false,
-})
+// const {Sequelize, DataTypes} = require ('sequelize');
+// // connection stablising with mysql
+// const sequelize = new Sequelize("inventory", "root", "mysql", {
+//     host: "localhost",
+//     dialect: "mysql",
+//     //port: "3307",
+//     logging: false,
+// })
 
-try {
-    sequelize.authenticate();
+// try {
+//     sequelize.authenticate();
   
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
 
-// module.exports = sequelize
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// // module.exports = sequelize
+// const db = {};
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
-db.sequelize.sync();
+// db.sequelize.sync();
 
-// db.products = require("../models/product")(sequelize, DataTypes);
-db.users = require("../Models/users")(sequelize, DataTypes);
+// // db.products = require("../models/product")(sequelize, DataTypes);
+// db.users = require("../Models/users")(sequelize, DataTypes);
 
-module.exports = db;
+// module.exports = db;
---
+++ Models/index.js
@@ -0,0 +1,27 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("shop", "root", "mysql", {
+  host: "localhost",
+  logging: false,
+  dialect: "mysql",
+  // operatorsAliases: false, 
+});
+
+try {
+    sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+  
+  const db = {};
+  
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+
+  db.users = require("./users")(sequelize, DataTypes);
+
+  db.sequelize.sync();
+
+  module.exports = db;
+
