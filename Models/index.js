9508e1b1392ff7143d7736dc58ac3ec8fdd41145
Rashid <rashid.h@copperdigital.com>
2023-05-26 14:46:34 +0530

    models added

--- Models/index.js
+++ Models/index.js
@@ -6,6 +6,8 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.book_borrower = require("../Models/book_borrower")(sequelize, DataTypes);
+db.books = require("../Models/books")(sequelize, DataTypes);
 db.admin = require("../Models/admin")(sequelize, DataTypes);
 db.users = require("../Models/users")(sequelize, DataTypes);
 
