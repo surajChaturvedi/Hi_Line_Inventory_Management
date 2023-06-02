dc0d934b365813b76a7ec16555d89d1f97788f59
shubhansu <shubhansu.c@copperdigital.com>
2023-06-02 11:45:49 +0530

    mail add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 
+
 //requiring connection
 require('./models/index.js');
 
--- routes/user/auth/signup.js
+++ routes/user/auth/signup.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {register,login , verifyOtp} = require('../../../controllers/auth');
+// const thePdfOutput = require('../../../pdfmaker/pdfmaker')
 
 router.post('/register', register);
 
 router.post('/login', login)
 
 router.post('/verify', verifyOtp );
+
+router.post('/resendOTPverification', )
+
+// router.get('/detailspdf', thePdfOutput );
 
 module.exports = router;
