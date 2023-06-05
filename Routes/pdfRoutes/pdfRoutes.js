4dfe1728c5bbe52153f8c27598782db8be9af2ff
Suraj <suraj.c@copperdigital.com>
2023-06-05 10:54:29 +0530

    edit pdf

--- Routes/pdfRoutes/pdfRoutes.js
+++ Routes/pdfRoutes/pdfRoutes.js
@@ -12,9 +12,6 @@ const db = require("../../Models/index");
 const admin = db.admin;
 const adminController = require("../../Controllers/adminController");
 
-
-
-
 const bookSearch = async (req, res) => {
         try {
           //get admin input
@@ -56,52 +53,5 @@ const bookSearch = async (req, res) => {
 
       router.get("/generateReport", bookSearch);
 
-  
-// let students = [
-//    {name: "Joy",
-//     email: "joy@example.com",
-//     city: "New York",
-//     country: "USA"},
-//    {name: "John",
-//     email: "John@example.com",
-//     city: "San Francisco",
-//     country: "USA"},
-//    {name: "Clark",
-//     email: "Clark@example.com",
-//     city: "Seattle",
-//     country: "USA"},
-//    {name: "Watson",
-//     email: "Watson@example.com",
-//     city: "Boston",
-//     country: "USA"},
-//    {name: "Tony",
-//     email: "Tony@example.com",
-//     city: "Los Angels",
-//     country: "USA"
-// }];
-
-
-// router.get("/generateReport", (req, res) => {
-//     ejs.renderFile(path.join(__dirname, '../../views/', "report-template.ejs"), {students: students}, (err, data) => {
-//     if (err) {
-//           res.send(err);
-//     } else {
-//         let options = {
-//             "height": "11.25in",
-//             "width": "8.5in",
-//             "header": {
-//                 "height": "20mm"
-//             },
-//             "footer": {
-//                 "height": "20mm",
-//             },
-//         };
-//         pdf.create(data, options).toFile("report.pdf", function (err, data) {
-//             if (err) {
-//                 res.send(err);
-//             } else {
-//                 res.send("File created successfully");
-//                    }
-//         }) }});});
 
         module.exports = router,{bookSearch};
